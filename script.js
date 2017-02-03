0d8901fb3eb478875f52ac0d2db72ef778553e02
lpaspb <lpaspb97@gmail.com>
2017-02-04 02:30:43 +0300

    redesign

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
-   
+   var letterArr = [];
+    var newStr = [];
     var callback = 1;
     
     $.getJSON("https://cors.now.sh/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=${callback}", function(data){
-            
-           $('.quote__par').html(JSON.stringify(data[0].content).replace(/\"/g, "").replace(/\\n/g, ""));
-            $('.quote__author').html(JSON.stringify(data[0].title).replace(/\"/g, ""));
+        
+         //var text = $('.quote__par').html(JSON.stringify(data[0].content).replace(/\"/g, "").replace(/\\n/g, ""));
+        var text = JSON.stringify(data[0].content).replace(/\"/g, "").replace(/\\n/g, "");
+        
+        letterArr = text.split("");
+        newStr = letterArr.join("");
+        console.log(letterArr.length);
+        if (letterArr.length > 90) {
+            newStr = newStr.slice(0, 120) + "..."
+            $('.quote__par').html(newStr);
+        } else {
+            $('.quote__par').html(JSON.stringify(data[0].content).replace(/\"/g, "").replace(/\\n/g, ""));
+}
+        
+        console.log(letterArr);
+        
+         $('.quote__author').html(JSON.stringify(data[0].title).replace(/\"/g, ""));
             
         $('#twitter').attr('href','https://twitter.com/intent/tweet?text=' + data[0].content + ' — ' + data[0].title);
         }, 'jsonp');
@@ -14,12 +29,25 @@ $(document).ready(function(){
     $('.quote__new').on('click', function() {
          callback++;
          console.log(callback);
+         
         $.getJSON("https://cors.now.sh/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + callback, function(data){
             
+             var text = JSON.stringify(data[0].content).replace(/\"/g, "").replace(/\\n/g, "");
+       
+        letterArr = text.split("");
+        newStr = letterArr.join("");
+        console.log(letterArr.length);
+        if (letterArr.length >= 90) {
+            newStr = newStr.slice(0, 120) + "..."
+            $('.quote__par').html(newStr);
+        } else {
             $('.quote__par').html(JSON.stringify(data[0].content).replace(/\"/g, "").replace(/\\n/g, ""));
+}
+        
+        console.log(letterArr);
             $('.quote__author').html(JSON.stringify(data[0].title).replace(/\"/g, ""));
               $('#twitter').attr('href','https://twitter.com/intent/tweet?text=' + data[0].content + ' — ' + data[0].title);
-        }, 'jsonp');
+        }, 'jsonp')
     });
     
      $('#twitter').on('click', function() {
@@ -27,7 +55,19 @@ $(document).ready(function(){
          
          $.getJSON("https://cors.now.sh/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + callback, function(data){
             
+             var text = JSON.stringify(data[0].content).replace(/\"/g, "").replace(/\\n/g, "");
+        
+        letterArr = text.split("");
+        newStr = letterArr.join("");
+        console.log(letterArr.length);
+        if (letterArr.length > 90) {
+            newStr = newStr.slice(0, 105) + "..."
+            $('.quote__par').html(newStr);
+        } else {
             $('.quote__par').html(JSON.stringify(data[0].content).replace(/\"/g, "").replace(/\\n/g, ""));
+}
+        
+        console.log(letterArr);
             $('.quote__author').html(JSON.stringify(data[0].title).replace(/\"/g, ""));
             
         
